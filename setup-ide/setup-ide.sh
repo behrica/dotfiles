db5c6e9f0065720e32720bf0ba286b34dc0463a5
Carsten Behring <carsten.behring@gmail.com>
2024-04-30 21:46:11 +0200

    Update setup-ide.sh

--- setup-ide/setup-ide.sh
+++ setup-ide/setup-ide.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf ~/.config/emacs/
+test -s "~/.config/emacs/" && exit
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom sync
 emacs --eval "(nerd-icons-install-fonts 1)(kill-emacs 0)"  --kill
