494d20e8a03f451d62a7f80ae2454b31fa677d27
Carsten Behring <carsten.behring@gmail.com>
2024-04-30 16:05:56 +0200

    aaded doom setup

---
+++ setup-ide/setup-ide.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf ~/.config/emacs/
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+~/.config/emacs/bin/doom sync
+emacs --eval \"(nerd-icons-install-fonts 1)(kill-emacs 0)\"  --kill
